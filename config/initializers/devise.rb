48425fd5d329f739cbfffa9981581ce9d8b9764a
GuskiS <edvards@lazdans.lv>
2016-09-25 00:47:04 +0300

    Changed confirmation and recover password field from :username to :email

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -136,7 +136,7 @@ Devise.setup do |config|
   config.reconfirmable = false
 
   # Defines which key will be used when confirming an account
-  config.confirmation_keys = [:username]
+  config.confirmation_keys = [:email]
 
   # ==> Configuration for :rememberable
   # The time the user will be remembered without asking for credentials again.
@@ -195,7 +195,7 @@ Devise.setup do |config|
   # ==> Configuration for :recoverable
   #
   # Defines which key will be used when recovering the password for an account
-  config.reset_password_keys = [:username]
+  config.reset_password_keys = [:email]
 
   # Time interval you can reset your password with a reset password key.
   # Don't put a too small interval or your users won't have the time to
