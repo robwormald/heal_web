cd26467aee193be46f58ceb199978d2ffbe9afb5
GuskiS <edvards@lazdans.lv>
2016-09-28 17:38:01 +0300

    Restored jquery, added vertical scroll for chat messages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require bootstrap-sprockets
 //= require core-js/client/shim.min.js
 //= require zone.js/dist/zone.js
 //= require reflect-metadata/Reflect.js
