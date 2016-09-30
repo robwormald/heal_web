1eb9e02a3913b70d91daeca3bbe54491ffe3a52a
GuskiS <edvards@lazdans.lv>
2016-09-30 13:55:11 +0300

    Fixed emojis for mobile and linux.

--- public/app/global/emoji-support.service.ts
+++ public/app/global/emoji-support.service.ts
@@ -4,17 +4,31 @@ import { Injectable } from '@angular/core';
 export class EmojiSupportService {
 
   init(): void {
-    if(this.isLinux() || !this.hasEmojiSupport()) {
-      var style = document.createElement('style');
+    let font = this.shouldAddSupport();
+
+    if(font.length) {
+      let style = document.createElement('style');
       style.type = 'text/css';
-      style.innerHTML = '.dynamic-no-emoji { font-family: "Noto Emoji, Apple Color Emoji, SegoeUIEmoji"; }';
+      style.innerHTML = `.dynamic-no-emoji { font-family: ${font}; }`;
       document.getElementsByTagName('head')[0].appendChild(style);
+    }
+  }
+
+  private shouldAddSupport(): string {
+    switch(true) {
+      case window['mobilecheck']():
+        return 'Noto Emoji, Apple Color Emoji, SegoeUIEmoji';
+      case this.isLinux():
+      case !this.hasEmojiSupport():
+        return 'SegoeUIEmoji';
+      default:
+        return '';
     }
   }
 
   private isLinux(): boolean {
     var version = navigator.appVersion;
-    return version.includes("X11") || version.includes('Linux');
+    return version.includes('X11') || version.includes('Linux');
   }
 
   private hasEmojiSupport(): boolean {
