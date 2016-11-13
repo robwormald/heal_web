7aa87113a9620a044f4b6316674b25dbc277b705
GuskiS <edvards@lazdans.lv>
2016-11-13 15:22:25 +0200

    Fixed modal windows

--- public/app/partials/modal/modal.component.ts
+++ public/app/partials/modal/modal.component.ts
@@ -2,16 +2,12 @@ import { Component, Input, ViewChild } from '@angular/core';
 
 @Component({
   moduleId: module.id,
-  selector: 'modal-partial',
+  selector: '[modal-partial]',
   templateUrl: './modal.component.html',
 })
 
 export class ModalPartialComponent {
   @Input('title') title;
   @Input('size') size = 'md';
-  @ViewChild('modalWindow') modalWindow;
-
-  show(): void {
-    this.modalWindow.show();
-  }
+  @ViewChild('modal') modal;
 }
