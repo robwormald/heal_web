1a00a1a0c341ceff52f988f6bf37b5f52c4a838d
GuskiS <edvards@lazdans.lv>
2016-11-13 20:57:33 +0200

    Added email format validation with regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   acts_as_voter
 
   alias_attribute :preference, :user_preference
 
   validates :username, presence: true, uniqueness: { case_sensitive: false }
-  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX, message: 'has invalid format' }
   validates :password, confirmation: true
   validates :password_confirmation, presence: true, if: 'password.present?'
 
