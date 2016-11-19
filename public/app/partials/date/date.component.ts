9355d3401a9c998f39def3651443d62eeca55531
GuskiS <edvards@lazdans.lv>
2016-11-19 02:12:04 +0200

    Added date pipe to date partial, added intl polyfill for older browsers

--- public/app/partials/date/date.component.ts
+++ public/app/partials/date/date.component.ts
@@ -1,12 +1,23 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   moduleId: module.id,
   selector: 'date-partial',
-  template: `<span class='date-partial'>{{ date }}</span>`,
+  template: `<span class='date-partial'>{{ date | date:format }}</span>`,
 })
 
-// | date:"y MMM d j:mm"
-export class DatePartialComponent {
+export class DatePartialComponent implements OnInit {
+  dateFormats: any = {
+    timeDate: 'j:mm, d. MMM y',
+    dateOnly: 'd. MMM y',
+    timeOnly: 'j:mm',
+  };
+  format: string = this.dateFormats.timeDate;
+
   @Input('date') date;
+  @Input('type') type;
+
+  ngOnInit(): void {
+    this.format = this.dateFormats[this.type] || this.format;
+  }
 }
