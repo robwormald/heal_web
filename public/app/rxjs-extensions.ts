e12155e6ac7ee2afb9bf3bed9657099ce57bc4af
GuskiS <edvards@lazdans.lv>
2016-11-20 21:53:01 +0200

    Removed some rxjs dependencies

--- public/app/rxjs-extensions.ts
+++ public/app/rxjs-extensions.ts
@@ -1,5 +1,3 @@
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/finally';
-import 'rxjs/add/operator/pluck';
-import 'rxjs/add/operator/distinctUntilChanged';
