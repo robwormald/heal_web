d9fef3154b493d41bc3676ecfe0a7bdd52e9510a
GuskiS <edvards@lazdans.lv>
2016-11-22 22:39:18 +0200

    Fixed appearance job

--- app/jobs/appearance/appearance_job.rb
+++ app/jobs/appearance/appearance_job.rb
@@ -2,9 +2,9 @@ class Appearance::AppearanceJob < ApplicationJob
   queue_as :default
 
   def perform(user, event)
-    user.update(online: event == 'join')
+    user.update(online: event == 'online_join')
     ActionCable.server.broadcast('home_all', ChannelHelpers.params(event, { user: User.parse(user, [:user]) }))
 
-    Appearance::UserListJob.perform_later(user) if event == 'join'
+    Appearance::UserListJob.perform_later(user) if event == 'online_join'
   end
 end
